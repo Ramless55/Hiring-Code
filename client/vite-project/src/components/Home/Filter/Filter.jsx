dda68c684ac39a268737a9c1751762be3455323f
Ezequiel Morales <ezemoralesmdp@gmail.com>
2022-08-11 23:25:09 -0300

    Retoque de Cards. Avance Footer. Stars

---
+++ client/vite-project/src/components/Home/Filter/Filter.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import './Filter.css'
+
+const Filter = () => {
+    return (
+        <div className="filter__container">
+            <ul className="filter__section">
+                <li className="footer__section-header">holaholaholaholahola</li>
+                <li><a href="#">holaholaholaholahola</a></li>
+                <li><a href="#">holaholaholaholahola</a></li>
+                <li><a href="#">holaholaholaholahola</a></li>
+                <li><a href="#">holaholaholaholahola</a></li>
+                <li><a href="#">holaholaholaholahola</a></li>
+                <li><a href="#">holaholaholaholahola</a></li>
+            </ul>
+        </div>
+    )
+}
+
+export default Filter
