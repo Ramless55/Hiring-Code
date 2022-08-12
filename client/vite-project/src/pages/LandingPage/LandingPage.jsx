64bc1836aed82596c74b4953ee7f61dc359341fb
Daniel Luna <daniel.alejandro.luna032@gmail.com>
2022-08-12 00:22:07 -0300

    Correction details.

--- client/vite-project/src/pages/LandingPage/LandingPage.jsx
+++ client/vite-project/src/pages/LandingPage/LandingPage.jsx
@@ -5,7 +5,7 @@ import './LandingPage.css'
 const LandingPage = () => {
   return (
     <div className='container__landing-page'>
-      <div className='content-1'>
+      <div className='landing__content-1'>
         <div className='filter-content-1'>
           <div className='section-header'>
             <div>
@@ -18,34 +18,40 @@ const LandingPage = () => {
               <a href="#sect-3">Novedades</a>
             </div>
           </div>
-          <div className='title-1'>
+          <div className='landing__content-1-title'>
             <h1>Anunciate <span className='title-span'>Ya!</span></h1>
             <h3>Anunciate en la página Nº1 en busqueda laboral y prestación de servivicios</h3>
           </div>
-          <div className='login-register'>
+          <div className='landing__content-login-register'>
               <p>Buscá tu empleo o brindá tu servicio ahora.</p>
-              <div>
-                <button>Ingresar</button>
-                <button>Registrarse</button>
+              <div className='container__button'>
+                <button 
+                className='button__login-register'>
+                Ingresar
+                </button>
+                <button 
+                className='button__login-register'>
+                Registrarse
+                </button>
               </div>
           </div>
         </div>
       </div>
 
-      <div className='content-2' id='sect-1'>
-          <h1>Titulo 2</h1>
-          <div className='cardsContent'>
-            <div className='card'>
+      <div className='landing__content-2' id='sect-1'>
+          <h1>Opiniones</h1>
+          <div className='container__cards'>
+            <div className='container__cards-card'>
                 <img src={cardImage} alt="Imagen" />
                 <h3>Titulo 1</h3>
                 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus iusto blanditiis ea. Magni, quaerat illum nulla, sit possimus inventore dolorum laborum est odio doloribus vel nobis sapiente modi rem ipsa.</p>
             </div>
-            <div className='card'>
+            <div className='container__cards-card'>
                 <img src={cardImage} alt="Imagen" />
                 <h3>Titulo 2</h3>
                 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus iusto blanditiis ea. Magni, quaerat illum nulla, sit possimus inventore dolorum laborum est odio doloribus vel nobis sapiente modi rem ipsa.</p>
             </div>
-            <div className='card'>
+            <div className='container__cards-card'>
                 <img src={cardImage} alt="Imagen" />
                 <h3>Titulo 3</h3>
                 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus iusto blanditiis ea. Magni, quaerat illum nulla, sit possimus inventore dolorum laborum est odio doloribus vel nobis sapiente modi rem ipsa.</p>
@@ -53,9 +59,9 @@ const LandingPage = () => {
           </div>
       </div>
 
-      <div className='content-3'>
-        <div className='content-3-first' id='sect-2'>
-          <div className='content-3-first-container'>
+      <div className='landing__content-3'>
+        <div className='landing__content-3-first' id='sect-2'>
+          <div className='landing__content-3-first-filter'>
             <div>
               <h2>Sample title</h2>
               <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Incidunt nam similique officia quidem sapiente culpa libero? Saepe quaerat, deleniti totam suscipit vel sint, quidem, alias quasi recusandae dolore libero aperiam?</p>
@@ -65,14 +71,14 @@ const LandingPage = () => {
             </div>
           </div>
         </div>
-        <div className='content-3-second'>
-          <div className='one'>
+        <div className='landing__content-3-second'>
+          <div className='content-3-second-box1'>
             <div>
               <h2>Sample title</h2>
               <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Incidunt nam similique officia quidem sapiente culpa libero? Saepe quaerat, deleniti totam suscipit vel sint, quidem, alias quasi recusandae dolore libero aperiam?</p>
             </div>
           </div>
-          <div className='two'>
+          <div className='content-3-second-box2'>
             <div>
               <h2>Sample title</h2>
               <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Incidunt nam similique officia quidem sapiente culpa libero? Saepe quaerat, deleniti totam suscipit vel sint, quidem, alias quasi recusandae dolore libero aperiam?</p>
@@ -81,22 +87,22 @@ const LandingPage = () => {
         </div>
       </div>
 
-      <div className='content-4' id='sect-3'>
+      <div className='landing__content-4' id='sect-3'>
         <h1>Novedades</h1>
-        <div className='container'>
-          <div className='card-animation'>
+        <div className='landing__content-4-container'>
+          <div className='content-4-card-animation'>
             <img src={cardImage} alt="Imagen" />
             <h3>Titulo 1</h3>
             <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Natus nesciunt neque, itaque quasi saepe voluptate doloribus quia qui omnis exercitationem incidunt magnam debitis molestias odio deleniti dolorem repudiandae iusto. Dolores.</p>
             <a href="#">Ver más</a>
           </div>
-          <div className='card-animation'>
+          <div className='content-4-card-animation'>
             <img src={cardImage} alt="Imagen" />
             <h3>Titulo 2</h3>
             <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Natus nesciunt neque, itaque quasi saepe voluptate doloribus quia qui omnis exercitationem incidunt magnam debitis molestias odio deleniti dolorem repudiandae iusto. Dolores.</p>
             <a href="#">Ver más</a>
           </div>
-          <div className='card-animation'>
+          <div className='content-4-card-animation'>
             <img src={cardImage} alt="Imagen" />
             <h3>Titulo 3</h3>
             <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Natus nesciunt neque, itaque quasi saepe voluptate doloribus quia qui omnis exercitationem incidunt magnam debitis molestias odio deleniti dolorem repudiandae iusto. Dolores.</p>
@@ -105,13 +111,13 @@ const LandingPage = () => {
         </div>
       </div>
 
-      <div className='content-final'>
+      <div className='landing__content-5'>
           <div>
             <h3>¿Aun no te decidís?</h3>
             <p>
               ¡Comenzá por registrarte para conseguir empleo o brindar tus servicios!
             </p>
-            <button className='button-register'>
+            <button className='content-5-button'>
               Registrarse
             </button>
           </div>
