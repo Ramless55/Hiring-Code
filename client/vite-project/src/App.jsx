f3d27e12e2fd5476e9ebac740e2dcde6483464c9
Tobias Gonzalez <103076990+TobiasGonzalezz@users.noreply.github.com>
2022-08-14 19:58:07 -0300

    Protected Routes

--- client/vite-project/src/App.jsx
+++ client/vite-project/src/App.jsx
@@ -1,7 +1,7 @@
 import './App.css'
 import './styles/main.css'
-import * as React from 'react';
-import { Routes, Route } from "react-router-dom";
+import {useState, useEffect} from 'react';
+import { Routes, Route, useLocation } from "react-router-dom";
 import Header from "./components/Header/Navbar"
 import HomePage from "./pages/HomePage/HomePage";
 import ErrorPage from "./pages/ErrorPage/ErrorPage";
@@ -10,21 +10,28 @@ import RegisterPage from './pages/RegisterPage/RegisterPage';
 import ProfilePage from './pages/ProfilePage/MyProfilePage/ProfilePage';
 import ProfileUsersPage from "./pages/ProfilePage/ProfileUserPage/ProfilePage"
 import Footer from './components/Footer/Footer'
-
+import ProtectedRoutes from './components/ProtectedRoutes/ProtectedRoutes';
 
 function App() {
+  const [isLogged, setIslogged] = useState(window.localStorage.getItem("Token"))
+  const location = useLocation()
+  
+  useEffect(()=> {
+    setIslogged(window.localStorage.getItem("Token"))
+  },[location])
+
   return (
     <div className="App">
-        <Header />
-        <Routes>
-          <Route path='/' element={<LandingPage />} />
-          <Route path='home' element={<HomePage />} />
-          <Route path='my-profile' element ={<ProfilePage />} />
-          <Route path='profile/:user' element={<ProfileUsersPage />} />
-          <Route path='register' element={<RegisterPage />} />
-          <Route path='*' element={<ErrorPage />} />
-        </Routes>
-        <Footer />
+      <Header />
+      <Routes>
+        <Route path='/' element={<LandingPage />} />
+        <Route path='my-profile' element={<ProtectedRoutes user={isLogged}> <ProfilePage /> </ProtectedRoutes> } />
+        <Route path='home' element={<HomePage />} />
+        <Route path='profile/:user' element={<ProfileUsersPage />} />
+        <Route path='register' element={<RegisterPage />} />
+        <Route path='*' element={<ErrorPage />} />
+      </Routes>
+      <Footer />
     </div>
   )
 }
