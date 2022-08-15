a84b687afb72e544d57d0371421fb8059d82faa6
Daniel Luna <daniel.alejandro.luna032@gmail.com>
2022-08-14 22:35:41 -0300

    Added github get cards

--- client/vite-project/src/components/AboutCard/AboutCard.jsx
+++ client/vite-project/src/components/AboutCard/AboutCard.jsx
@@ -6,40 +6,96 @@ import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
 import { CardActionArea } from '@mui/material';
+import { height } from '@mui/system';
+import { useState, useEffect } from 'react';
+import axios from 'axios'
 
 
 
 
-const AboutCard = (props) => {
+const AboutCard = ({ user }) => {
+    const [info, setInfo] = useState({})
+
+    const githubUser = () => {
+        axios.get(`https://api.github.com/users/${user}`)
+            .then(res => setInfo(res.data))
+    }
+
+    const visitGithub = () => {
+        window.open(info.html_url, "_blank")
+    }
+
+    useEffect(() => {
+        console.log(info)
+    }, [info])
+
+    useEffect(() => {
+        githubUser()
+    }, [])
+
     return (
         <div className='aboutCard__container'>
             <Card sx={{
-                maxWidth: 345,
+                minWidth: 400,
+                maxWidth: 400,
                 borderRadius: "20px",
+                boxShadow: "0 0 10px rgba(0,0,0,0.7)",
+                // display:"flex",
+                // flexDirection:"column",
+                // justifyContent:"center",
+                // alingItems:"center",
             }}>
-                <CardMedia
-                    component="img"
-                    height="300"
-                    image={props.image}
-                    alt="green iguana"
-                />
-                <CardContent>
-                    <Typography
-                        gutterBottom
-                        variant="h5"
-                        component="div"
+                <CardActionArea onClick={visitGithub}>
+                    <CardMedia
+                        component="img"
+                        height="300"
+                        image={info.avatar_url}
+                        alt="programmer image"
                         sx={{
-                            display: "flex",
-                            alingItems: "center",
-                            justifyContent: "center",
+                            maxHeight: '300px',
                         }}
-                    >
-                        {props.title}
-                    </Typography>
-                    <Typography variant="body2" color="text.secondary">
-                        {props.description}
-                    </Typography>
-                </CardContent>
+                    />
+                    <CardContent>
+                        <Typography
+                            gutterBottom
+                            variant="h5"
+                            component="div"
+                            sx={{
+                                display: "flex",
+                                alingItems: "center",
+                                justifyContent: "center",
+                            }}
+                        >
+                            {info.name}
+                        </Typography>
+                        <Typography
+                            variant="body2"
+                            color="text.secondary"
+                            sx={{
+                                fontFamily: "Roboto",
+                                color: "var(--font-dark)",
+                                height: "5vh",
+                            }}
+                        >
+                            {info.bio && 
+                                info.bio.includes(':coffee:') ?
+                                info.bio.split(':coffee:')[1]
+                                : info.bio
+                            }
+                        </Typography>
+                        <Typography
+                            variant="body2"
+                            color="text.secondary"
+                            sx={{
+                                fontFamily: "Roboto",
+                                color: "var(--font-dark)",
+                                height: "5vh",
+                            }}
+                        >
+                            {info.location}
+                        </Typography>
+                    </CardContent>
+                </CardActionArea>
             </Card>
         </div>
     )
--- client/vite-project/src/pages/AboutPage/AboutPage.jsx
+++ client/vite-project/src/pages/AboutPage/AboutPage.jsx
@@ -1,51 +1,89 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import AboutCard from '../../components/AboutCard/AboutCard'
 import imgDani from '../../assets/img/about-us__card-dani.png'
+import axios from 'axios'
 import './AboutPage.css'
 
-const AboutPage = (props) => {
-  return (
-    <div className='aboutPage__container'>
-        <div className='aboutPage__title'>
-            <div>
-                <h1>Acerca de Nosotros</h1> 
-            </div>
-        </div>
-        <div className='aboutPage__container-cards'>
-            <div className='aboutPage__container-cards-grid'>
-                <div className='aboutPage__container-cards-box'>
-                    <AboutCard
-                        image={imgDani}
-                        title={'Dani'} 
-                        description={'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Accusantium nulla aspernatur quos maiores totam, repellat non perferendis omnis et exercitationem consectetur recusandae alias necessitatibus nobis at corporis iste quaerat explicabo?'}
-                    />
-                </div>
-                <div className='aboutPage__container-cards-box'>
-                    <AboutCard
-                        image={imgDani}
-                        title={'Dani'}
-                        description={'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Accusantium nulla aspernatur quos maiores totam, repellat non perferendis omnis et exercitationem consectetur recusandae alias necessitatibus nobis at corporis iste quaerat explicabo?'} 
-                    />
-                </div>
-                <div className='aboutPage__container-cards-box'>
-                    <AboutCard
-                        image={imgDani}
-                        title={'Dani'}
-                        description={'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Accusantium nulla aspernatur quos maiores totam, repellat non perferendis omnis et exercitationem consectetur recusandae alias necessitatibus nobis at corporis iste quaerat explicabo?'} 
-                    />
-                </div>
-                <div className='aboutPage__container-cards-box'>
-                    <AboutCard
-                        image={imgDani}
-                        title={'Dani'}
-                        description={'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Accusantium nulla aspernatur quos maiores totam, repellat non perferendis omnis et exercitationem consectetur recusandae alias necessitatibus nobis at corporis iste quaerat explicabo?'} 
-                    />
+
+const AboutPage = () => {
+    const [user1, setUser1] = useState({})
+    const [user2, setUser2] = useState({})
+    const [user3, setUser3] = useState({})
+    const [user4, setUser4] = useState({})
+
+    const users = ['daniluna032', 'Ramless55', 'TobiasGonzalezz', 'ezemoralesmdp']
+
+
+    const githubUser = () => {
+        axios.get(`https://api.github.com/users/${users[0]}`)
+            .then(res => setUser1(res.data))
+
+        axios.get(`https://api.github.com/users/${users[1]}`)
+            .then(res => setUser2(res.data))
+
+        axios.get(`https://api.github.com/users/${users[2]}`)
+            .then(res => setUser3(res.data))
+
+        axios.get(`https://api.github.com/users/${users[3]}`)
+            .then(res => setUser4(res.data))
+    }
+
+    useEffect(() => {
+        githubUser()
+    }, [])
+
+
+    return (
+        <div className='aboutPage__container'>
+            <div className='aboutPage__title'>
+                <div>
+                    <h1>Acerca de Nosotros</h1>
                 </div>
             </div>
+            <div className='aboutPage__container-cards'>
+                <div className='aboutPage__container-cards-grid'>
+                    {users.map(user => {
+                        return (
+                            <div className='aboutPage__container-cards-box' key={user}>
+                                <AboutCard
+                                    user={user}
+                                />
+                            </div>
+                        )
+                    })}
+                    {/* <div className='aboutPage__container-cards-box'>
+                    <AboutCard
+                        image={user1.avatar_url}
+                        title={user1.name} 
+                        description={user1.bio}
+                    />
+                </div>
+                <div className='aboutPage__container-cards-box'>
+                    <AboutCard
+                        image={user2.avatar_url}
+                        title={user2.name}
+                        description={user2.bio} 
+                    />
+                </div>
+                <div className='aboutPage__container-cards-box'>
+                    <AboutCard
+                        image={user3.avatar_url}
+                        title={user3.name}
+                        description={user3.bio} 
+                    />
+                </div>
+                <div className='aboutPage__container-cards-box'>
+                    <AboutCard
+                        image={user4.avatar_url}
+                        title={user4.name}
+                        description={user4.bio} 
+                    />
+                </div> */}
+                </div>
+            </div>
+
         </div>
-        
-    </div>
-  )
+    )
 }
 
 export default AboutPage
