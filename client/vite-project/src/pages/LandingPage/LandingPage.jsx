21899fda83e9bf09ec84c6de1a58b0b43d8fdf5a
Daniel Luna <daniel.alejandro.luna032@gmail.com>
2022-08-17 19:39:27 -0300

    Better changes :p

--- client/vite-project/src/pages/LandingPage/LandingPage.jsx
+++ client/vite-project/src/pages/LandingPage/LandingPage.jsx
@@ -5,6 +5,7 @@ import AnimatedPage from '../../components/AnimatedPage/AnimatedPage'
 import LoginButton from '../../components/Button/LandingButton/LandingButtonLogin'
 import RegisterButton from '../../components/Button/LandingButton/LandingButtonRegister'
 import './LandingPage.css'
+import landing_news from '../../assets/img/landing_news.jpg' 
 
 const LandingPage = () => {
 
@@ -71,8 +72,53 @@ const LandingPage = () => {
           </div>
         </div>
 
+        <motion.div className='landing__content-3' initial={{ opacity: 0 }} whileInView={{ opacity: 1 }}>
+          <div className='landing__content-3-first' id='sect-2'>
+            <div className='landing__content-3-first-filter'>
+              <div>
+                <h2>¿Por qué nosotros?</h2>
+                <p>Nuestra plataforma es intuitiva y centrada en la facilidad de buscar empleo en la industia de TI</p>
+                <a className='button__template' href='#info-target'>+ INFO</a>
+              </div>
+            </div>
+          </div>
+          <motion.div className='landing__content-3-second' id='info-target'
+            initial={{ opacity: 0, overflow: "hidden" }}
+            whileInView={{ opacity: 1, overflow: "hidden" , transition: { ease: 'easeInOut', duration: 0.5 }}}
+            viewport={{ once: true , amount: 0.8}}
+          >
+            <div className='content-3-second-box1'>
+              <motion.div
+                initial={{ opacity: 0, x: -200, }}
+                whileInView={{ opacity: 1, x: 0}}
+                transition={{ ease: 'easeInOut', duration: 0.5 }}
+                animate={{once: true,}}
+                // viewport={{once: true,}}
+                >
+                
+                <h2>Comunidad</h2>
+                <p>Tenemos una comunidad de personas involucradas en la industria de TI que está en constante crecimiento. No te prives de ser parte de nosotros y conseguir tu puesto en la industria Tech</p>
+              </motion.div>
+            </div>
+            <div className='content-3-second-box2'>
+              <motion.div
+                initial={{ opacity: 0, x: 200, }}
+                whileInView={{ opacity: 1, x: 0, }}
+                transition={{ ease: 'easeInOut', duration: 0.5 }}>
+                <h2>Soporte 24/7</h2>
+                <p>Nuestro equipo de desarrolladores esta pendiente de cualquier duda con respecto a la página las 24 hs.</p>
+              </motion.div>
+            </div>
+          </motion.div>
+        </motion.div>
+
         <div className='landing__content-4' id='sect-3'>
-          <h1>Novedades</h1>
+          <div className='landing__content-4-title'>
+            <div className='title-container-news'>
+              <h1>Novedades</h1>
+            </div>
+            <div className='title-image'></div>
+          </div>
           <div className='landing__content-4-container'>
             <div className='content-4-card-animation'>
               <img src={cardImage} alt="Imagen" />
@@ -94,49 +140,6 @@ const LandingPage = () => {
             </div>
           </div>
         </div>
-
-
-        <motion.div
-          className='landing__content-3'
-          initial={{ opacity: 0 }}
-          whileInView={{ opacity: 1 }}>
-          <div className='landing__content-3-first' id='sect-2'>
-            <div className='landing__content-3-first-filter'>
-              <div>
-                <h2>Sample title</h2>
-                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Incidunt nam similique officia quidem sapiente culpa libero? Saepe quaerat, deleniti totam suscipit vel sint, quidem, alias quasi recusandae dolore libero aperiam?</p>
-                <button className='button-more-info'>+ Info</button>
-              </div>
-            </div>
-          </div>
-          <motion.div className='landing__content-3-second'
-            initial={{ opacity: 0, overflow: "hidden" }}
-            whileInView={{ opacity: 1, overflow: "hidden" }}
-            transition={{ ease: 'easeInOut', duration: 1 }}
-          >
-            <div className='content-3-second-box1'>
-              <motion.div
-                initial={{ opacity: 0, x: -200, }}
-                whileInView={{ opacity: 1, x: 0, }}
-                transition={{ ease: 'easeInOut', duration: 1 }}>
-                <h2>Sample title</h2>
-                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Incidunt nam similique officia quidem sapiente culpa libero? Saepe quaerat, deleniti totam suscipit vel sint, quidem, alias quasi recusandae dolore libero aperiam?</p>
-              </motion.div>
-            </div>
-            <div
-              className='content-3-second-box2'
-            >
-
-              <motion.div
-                initial={{ opacity: 0, x: 200, }}
-                whileInView={{ opacity: 1, x: 0, }}
-                transition={{ ease: 'easeInOut', duration: 1 }}>
-                <h2>Sample title</h2>
-                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Incidunt nam similique officia quidem sapiente culpa libero? Saepe quaerat, deleniti totam suscipit vel sint, quidem, alias quasi recusandae dolore libero aperiam?</p>
-              </motion.div>
-            </div>
-          </motion.div>
-        </motion.div>
 
         <div className='landing__content-2' id='sect-1'>
           <div className='landing__content-2-title'>
