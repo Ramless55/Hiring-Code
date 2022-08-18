0eac96095a88fdcda9547b41db469135eca553ad
Ramiro Morales Kraff <ramiromoraleskraff@gmail.com>
2022-08-18 07:03:00 -0300

    La llegada de dios a la tiera

--- client/vite-project/src/pages/ErrorPage/ErrorPage.jsx
+++ client/vite-project/src/pages/ErrorPage/ErrorPage.jsx
@@ -16,40 +16,39 @@ const ErrorPage = () => {
         return () => document.removeEventListener("mousemove", mouseFunction)
     }, [])
 
-<return (
-        <>
-            <div className='container'>
-                <div className='content'>
-                    <div className='contentFlex'>
-                        <div className='cursorShadow' style={{
-                            position: "absolute",
-                            width: "0.01px",
-                            height: "0.01px",
-                            border: "none",
-                            background: "rgba(255,255,255,.1)",
-                            borderRadius: "50%",
-                            boxShadow: "0px 0px 200px 100px rgba(255,255,255,.1)"
-                        }}></div>
-                        <h1>
-                            No se encontró la página
-                        </h1>
-                        <h5>Mmm, la página que está s buscando parece que ya no existe.</h5>
-                        <Button className="button" variant="outlined" href="/" sx={{
-                            bgcolor: "white",
-                            color: "black",
-                            borderColor: "white",
-                            '&:hover': {
-                                bgcolor: "inherit",
-                                color: 'white',
-                                borderColor: "inherit"
-                            }
-                        }}>Volver al inicio</Button>
+        return (
+            <>
+                <div className='container'>
+                    <div className='content'>
+                        <div className='contentFlex'>
+                            <div className='cursorShadow' style={{
+                                position: "absolute",
+                                width: "0.01px",
+                                height: "0.01px",
+                                border: "none",
+                                background: "rgba(255,255,255,.1)",
+                                borderRadius: "50%",
+                                boxShadow: "0px 0px 200px 100px rgba(255,255,255,.1)"
+                            }}></div>
+                            <h1>
+                                No se encontró la página
+                            </h1>
+                            <h5>Mmm, la página que está s buscando parece que ya no existe.</h5>
+                            <Button className="button" variant="outlined" href="/" sx={{
+                                bgcolor: "white",
+                                color: "black",
+                                borderColor: "white",
+                                '&:hover': {
+                                    bgcolor: "inherit",
+                                    color: 'white',
+                                    borderColor: "inherit"
+                                }
+                            }}>Volver al inicio</Button>
+                        </div>
                     </div>
                 </div>
-            </div>
-        </>
-
-    )
+            </>
+        )
 }
 
 export default ErrorPage
